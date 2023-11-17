2c54a2ff8e3fcb043c74fabd6b10f9659a68f85f
JoelCourtney <joel.e.courtney@gmail.com>
2023-11-16 20:14:17 -0800

    Reduce map overloads

--- timeline/src/main/kotlin/gov/nasa/jpl/aerie/timeline/ops/ProfileOps.kt
+++ timeline/src/main/kotlin/gov/nasa/jpl/aerie/timeline/ops/ProfileOps.kt
@@ -11,18 +11,12 @@ interface ProfileOps<V : Any, P: Any>: TimelineOps<Segment<V>, P> {
   fun assignGaps(other: ProfileOps<V, P>) = other.set(this)
   fun set(other: ProfileOps<V, P>) = map2Values(other, BinaryOperation.combineOrIdentity { _, r, _ -> r })
 
-  fun mapValues(f: (Segment<V>) -> V) = mapValuesInternal(ctor, f)
-  fun mapValuesWindows(f: (Segment<V>) -> Boolean) = mapValuesInternal(::Windows, f)
-  fun mapValuesReal(f: (Segment<V>) -> RealDynamics) = mapValuesInternal(::Real, f)
-  fun <W : Any> mapValuesDiscrete(f: (Segment<V>) -> W) = mapValuesInternal(::Discrete, f)
-  private fun <W: Any, Q: Any> mapValuesInternal(ctor: (TimelineOps<Segment<W>, Q>) -> Q, f: (Segment<V>) -> W) =
+  fun mapValues(f: (Segment<V>) -> V) = mapValuesInto(ctor, f)
+  fun <W: Any, PInto: Any> mapValuesInto(ctor: (TimelineOps<Segment<W>, PInto>) -> PInto, f: (Segment<V>) -> W) =
       Timeline(ctor) { bounds -> collect(bounds).map { it.mapValue(f) }}.specialize()
 
-  fun <W: Any, Q: Any> map2Values(other: ProfileOps<W, Q>, op: BinaryOperation<V, W, V?>) = map2ValuesInternal(ctor, other, op)
-  fun <W: Any, Q: Any> map2ValuesWindows(other: ProfileOps<W, Q>, op: BinaryOperation<V, W, Boolean?>) = map2ValuesInternal(::Windows, other, op)
-  fun <W: Any, Q: Any> map2ValuesReal(other: ProfileOps<W, Q>, op: BinaryOperation<V, W, RealDynamics?>) = map2ValuesInternal(::Real, other, op)
-  fun <W: Any, Q: Any, Out: Any> map2ValuesDiscrete(other: ProfileOps<W, Q>, op: BinaryOperation<V, W, Out?>) = map2ValuesInternal(::Discrete, other, op)
-  private fun <W: Any, Q: Any, Out: Any, R: Any> map2ValuesInternal(ctor: (TimelineOps<Segment<Out>, R>) -> R, other: ProfileOps<W, Q>, op: BinaryOperation<V, W, Out?>) =
+  fun <W: Any, Q: Any> map2Values(other: ProfileOps<W, Q>, op: BinaryOperation<V, W, V?>) = map2ValuesInto(ctor, other, op)
+  fun <W: Any, POther: Any, Out: Any, PInto: Any> map2ValuesInto(ctor: (TimelineOps<Segment<Out>, PInto>) -> PInto, other: ProfileOps<W, POther>, op: BinaryOperation<V, W, Out?>) =
       Timeline(ctor) { bounds -> map2Lists(collect(bounds), other.collect(bounds), op) }.specialize()
 
   fun filter(f: (Segment<V>) -> Boolean) = Timeline(ctor) { bounds -> collect(bounds).filter(f) }.specialize()
